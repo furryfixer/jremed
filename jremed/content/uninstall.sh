8febad97d37d4d5de058bdb784b0b0fb5f3751e4
William Rueger <spammer.victim@gmail.com>
2022-10-12 19:31:04 -0400

    Add files via upload

---
+++ jremed/content/uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# user must be root
+if [ "$(id -u)" != "0" ]; then
+   echo "Uninstallation must be run as root" 1>&2
+   exit 1
+fi
+
+# remove any existing files and folders
+echo Removing application...
+echo
+rm -r -v -f /opt/jremed
+rm -v /usr/share/applications/jremed.desktop
+rm -v /usr/bin/jremed
+rm -v /bin/jremed
+echo
+echo Application successfully removed.
