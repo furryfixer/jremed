3ed5c8142e7cd3346f800d7d3e044af59d026fd3
William Rueger <spammer.victim@gmail.com>
2026-07-31 15:51:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 ##########################################################################
-# This script installs jremed application to your system. It adds
-# a shortcut to your main menu in
-# Preferences -> JRE Menu Editor
-# or
+echo "
+# This script installs the jremed application to your system.
+# It adds a shortcut to your main menu in
+# Preferences -> JRE Menu Editor, or
 # System -> Preferences -> JRE Menu Editor
-# depending on your distribution
+# depending on your distribution.
 #
-# Modified from lxmed by William Rueger (furryfixer), 2022
+# Modified from lxmed by William Rueger (furryfixer), 2026
 # Original Author: Marko Čičak
-##########################################################################
 
+Press <Enter> to continue
+"
+##########################################################################
+read a
 # user must be root
 if [ "$(id -u)" != "0" ]; then
-   echo "Installation must be run as root" 1>&2
+   echo "STOP! Installation must be run as root
+" 1>&2
    exit 1
 fi
 
@@ -23,20 +27,28 @@ echo
 # remove any existing files and folders
 echo Removing any previous installed files and folders...
 rm -r -v -f /opt/jremed
-rm -v /usr/share/applications/jremed.desktop
-rm -v /usr/bin/jremed
-rm -v /usr/share/polkit-1/actions/org.pkexec.jremed.policy
-
+rm -f /usr/share/applications/jremed.desktop
+rm -f /usr/bin/jremed*
+rm -f /usr/share/polkit-1/actions/org.pkexec.jremed.policy
+echo 
 # copy new files
-mkdir -v /opt/jremed
 cp -v content/jremed /usr/bin/
 chmod -v +x /usr/bin/jremed
-cp -v content/JREmenuEditor.jar /opt/jremed/
-cp -v content/uninstall.sh /opt/jremed/
-chmod -v +x /opt/jremed/uninstall.sh
-cp -v content/lxmed.png /opt/jremed/
+cp -v content/jremed-bin /usr/bin/
+chmod -v +x /usr/bin/jremed-bin
 cp -v content/jremed.desktop /usr/share/applications/
 cp -v content/org.pkexec.jremed.policy /usr/share/polkit-1/actions/
+mkdir -p -v /opt/jremed
+cp -v README.md /opt/jremed/
+cp -v LICENSE /opt/jremed/
+cp -v content/JREmenuEditor.jar /opt/jremed/
+cp -v content/lxmed.png /opt/jremed/
+cp -v content/uninstall.sh /opt/jremed/
+chmod -v +x /opt/jremed/uninstall.sh
+touch /var/log/jremed.log
+chmod 0666 /var/log/jremed.log
 echo
-echo "Installation sucessfully completed. Enter jremed to run application or check Preferences -> JRE Menu Editor in your main menu"
-echo
+echo "Installation sucessfully completed. Enter jremed to run  application
+or check Preferences -> JRE Menu Editor in your main menu
+"
+exit
